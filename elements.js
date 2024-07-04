bc930e7952602587a5e8f397e30a6076ef8dee17
trias <triasnugra97@gmail.com>
2024-07-04 21:30:16 +0700

    Update elements.js

--- elements.js
+++ elements.js
@@ -4,6 +4,7 @@ const elements = [
         "symbol": "H",
         "number": 1,
         "mass": "1.008",
+        "category": "Non-metal",
         "position": "1 / 1 / 2 / 2"
     },
     {
@@ -11,6 +12,7 @@ const elements = [
         "symbol": "He",
         "number": 2,
         "mass": "4.0026",
+        "category": "Noble gas",
         "position": "1 / 18 / 2 / 19"
     },
     // Add more elements here

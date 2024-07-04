4201543e6b803f1b7bab9e95abfd54a37cad9195
trias <triasnugra97@gmail.com>
2024-07-04 21:26:57 +0700

    Create elements.js

---
+++ elements.js
@@ -0,0 +1,17 @@
+const elements = [
+    {
+        "name": "Hydrogen",
+        "symbol": "H",
+        "number": 1,
+        "mass": "1.008",
+        "position": "1 / 1 / 2 / 2"
+    },
+    {
+        "name": "Helium",
+        "symbol": "He",
+        "number": 2,
+        "mass": "4.0026",
+        "position": "1 / 18 / 2 / 19"
+    },
+    // Add more elements here
+];
